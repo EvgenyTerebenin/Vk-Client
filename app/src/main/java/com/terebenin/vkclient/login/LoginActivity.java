adca7da107b781fa349372d7f08a2fd6991a9b19
Evgeny Terebenin <terebenin.e.p@gmail.com>
2017-01-24 16:20:18 +0300

    auth works

--- app/src/main/java/com/terebenin/vkclient/login/LoginActivity.java
+++ app/src/main/java/com/terebenin/vkclient/login/LoginActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.terebenin.vkclient.R;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 import com.vk.sdk.util.VKUtil;
 
 import butterknife.BindView;
@@ -19,6 +24,22 @@ import butterknife.ButterKnife;
  */
 public class LoginActivity extends Activity {
 
+    private boolean isResumed = false;
+
+    /**
+     * Scope is set of required permissions for your application
+     *
+     * @see <a href="https://vk.com/dev/permissions">vk.com api permissions documentation</a>
+     */
+    private static final String[] sMyScope = new String[]{
+            VKScope.FRIENDS,
+            VKScope.WALL,
+            VKScope.PHOTOS,
+            VKScope.NOHTTPS,
+            VKScope.MESSAGES,
+            VKScope.DOCS
+    };
+
     @BindView(R.id.uiBtnSignIn) Button btnSignIn;
 
     @Override
@@ -27,12 +48,38 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.layout_login);
         ButterKnife.bind(this);
 
+        VKSdk.wakeUpSession(this, new VKCallback<VKSdk.LoginState>() {
+            @Override
+            public void onResult(VKSdk.LoginState res) {
+//                if (isResumed) {
+                    switch (res) {
+                        case LoggedOut:
+                            Toast.makeText(LoginActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
+//                            showLogin();
+                            break;
+                        case LoggedIn:
+                            Toast.makeText(LoginActivity.this, "Logged In", Toast.LENGTH_SHORT).show();
+//                            showLogout();
+                            break;
+                        case Pending:
+                            break;
+                        case Unknown:
+                            break;
+                    }
+//                }
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        });
+
         btnSignIn.setOnClickListener((View view) -> {
-            //TODO
+            VKSdk.login(this, sMyScope);
         });
 
         String[] fingerprint = VKUtil.getCertificateFingerprint(this, this.getPackageName());
         Log.d("Fingerprint", fingerprint[0]);
     }
-
 }
